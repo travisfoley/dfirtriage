0a5a4ccaf7bd258ab89710e34dbe8b0706a9ed80
travisfoley <travisfoley@users.noreply.github.com>
2016-06-01 14:10:39 -0500

    Update DFIRTriage-pub_2.4.py
    Cleaned up top of file comments.

--- source/DFIRTriage-pub_2.4.py
+++ source/DFIRTriage-pub_2.4.py
@@ -2,39 +2,15 @@
 
 #############################################################################
 ##                                                                         ##
-## Unlicense                                                               ##
-## No Copyright, Project 0x7                                               ##
-##                                                                         ##
-## This is free and unencumbered software released into the public domain. ##
-## Anyone is free to copy, modify, publish, use, compile, sell, or         ##
-## distribute this software, either in source code form or as a compiled   ##
-## binary, for any purpose, commercial or non-commercial, and by any       ##
-## means.                                                                  ##
-##                                                                         ##
-## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,         ##
-## EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF      ##
-## MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  ##
-## IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR       ##
-## OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,   ##
-## ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR   ##
-## OTHER DEALINGS IN THE SOFTWARE.                                         ##
-##                                                                         ##
-##  For more information, please refer to  http://unlicense.org            ##
-##                                                                         ##
-##  Author: Travis Foley, travis.foley@gmail.com                           ##
-##                                                                         ##
-#############################################################################
-
-#############################################################################
-##                                                                         ##
-## DESCRIPTION: Forensic acquisition of volatile data and system           ##
+## Description: Forensic acquisition of volatile data and system           ##
 ## information for use with initial Incident Response.                     ##
 ##                                                                         ##
-## FILENAME: DFIRTriage.py                                                 ##
-## LAST MODIFIED: 5-13-16                                                  ##
-## VERSION: 2.4.1                                                          ##
-## STATUS: PUBLIC                                                          ##
+## License: Unlicense (http://unlicense.org)                               ##
 ##                                                                         ##
+## Version: 2.4                                                            ##
+## Filename: DFIRTriage.py                                                 ##
+## Author: Travis Foley, travis.foley@gmail.com                            ##
+## Last modified: : 4-20-16                                                ##
 ##                                                                         ##
 #############################################################################
 
